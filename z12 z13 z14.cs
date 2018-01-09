f5bef496ee46521590443abe04fce31dd130d5ed
KarolCzubI31 <33674865+KarolCzubI31@users.noreply.github.com>
2018-01-10 00:53:45 +0100

    Add files via upload

---
+++ z12 z13 z14.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace lab3
+{
+    class Program
+    {
+        public struct TResultDataStructure //zad12
+        {
+            public int a;
+            public int A 
+            {
+                get { return a; }
+                set { a = value; }
+            }
+            public int b;
+            public int B {
+                get { return b; }
+                set { b = value; }
+            }
+        }
+
+        public Task<bool> Zadanie2() //zad13
+        {
+            return Task.Run(() =>
+                    {
+                        bool Z2 = true;
+                        return Z2;
+                    });
+        }
+
+        public static async Task<XmlDocument> Zadanie3(string address) //zad14
+        {
+            WebClient webClient = new WebClient();
+            string xmlContent = await webClient.DownloadStringTaskAsync(new Uri(address));
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlContent);
+            return doc;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Zadanie3("http://www.feedforall.com/sample.xml").Result.InnerText); //wywołanie zad14
+
+            Console.ReadKey();
+        }
+    }
+}
