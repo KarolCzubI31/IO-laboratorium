935cf05e50ec30ed43ea4df1fba5df790a1586be
KarolCzubI31 <33674865+KarolCzubI31@users.noreply.github.com>
2018-01-10 00:42:04 +0100

    Add files via upload

---
+++ Lab1 Zad1-5/z1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace z1
+{
+    class Program
+    {
+		static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(ThreadProc, new object[] { 1000 });
+            Thread.Sleep(3000);
+        }
+
+        static void ThreadProc(Object stateInfo)
+        {
+            var time = ((object[])stateInfo)[0];
+            Thread.Sleep((int)time);
+            Console.WriteLine("watek poczekal " + time + " ms");
+        }
+    }
+}
